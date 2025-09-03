f81ee85fb10d25452dddcda4e442bad5cd7d46be
Nghia Vu <nghiavu@Nghias-MacBook-Pro.local>
2025-09-02 19:52:01 -0500

    Set up background color

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 // app/page.tsx
@@ -5,9 +6,9 @@ export default function HomePage() {
   return (
     <>
       <h1 className="text-3xl underline">Welcome to Resume Analyzer Rupert!</h1>
-      <Link href={"/sign-in"}>
-        <button className="bg-blue-500 text-white">Click me</button>
-      </Link>
+      <Button>
+        <Link href={"/sign-in"}>Click me</Link>
+      </Button>
     </>
       
     ) 
