70b0481dea6d85f04286cf3f6352b90fe35831b3
Mission Kadariya <mis8202@hytrol.com>
2025-09-17 16:43:15 -0500

    Added docker-postgres

---
+++ lib/db.ts
@@ -0,0 +1,12 @@
+import { Pool } from 'pg';
+
+const pool = new Pool ({
+
+    user: process.env.DB_USER as string,
+    host: process.env.DB_HOST as string,
+    database: process.env.DB_NAME as string,
+    password: process.env.DB_PASSWORD as string,
+    port: parseInt(process.env.DB_PORT as string, 10)
+})
+
+export default pool;
