78368a7024506ed3bc2bd0284af7ea4f84166b1d
Nghia <nghiavu144@gmail.com>
2025-09-24 19:14:53 -0500

    Making LLM work using Googe Gemini Instead

--- app/(dashboard)/dashboard/userform/page.tsx
+++ app/(dashboard)/dashboard/userform/page.tsx
@@ -19,7 +19,23 @@ const page = () => {
     
     const data = await response.json()
     console.log('Response data after sending to back end:', data)
-    router.push('/dashboard/analysis')
+    // router.push('/dashboard/analysis')
+    console.log('Data:', data.data.resumeText)
+
+    // Test out our LLM Gemini
+    const payload = {
+      companyName: data.data.companyName,
+      jobTitle: data.data.jobTitle,
+      jobDescription: data.data.jobDescription,
+      resumeText: data.data.resumeText
+    }
+    const response2 = await fetch('/api/data-analysis', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ resumeDetails: payload })
+    })
+    const data2 = await response2.json()
+    console.log('Response received from data analysis:', data2)
   }
   return (
     <ResumeForm onSubmit={onSubmit} isLoading={isLoading}/>
